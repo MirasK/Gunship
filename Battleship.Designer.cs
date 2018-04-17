48fb23d0aeda3cd85718ba77fa7331f7b2f27f98
Miras Kuandyk <mirkrokok0@gmail.com>
2018-04-17 22:34:22 +0600

    Battleship

---
+++ Battleship.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BattleShip
+{
+    partial class main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(890, 28);
+            this.menuStrip.TabIndex = 4;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.StartGame);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 580);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "main";
+            this.Text = "BattleShip";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+    }
+}
---
+++ Game.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BattleShip
+{
+    enum PlayerType
+    {
+        human,
+        bot
+    }
+
+    class Game
+    {
+        public PlayerPanel player1;
+        public PlayerPanel player2;
+
+        public bool isEnded;
+
+        public Game(PlayerType pl1, PlayerType pl2)
+        {
+            player1 = new PlayerPanel(pl1, PanelPos.left, BotTurn, GameOver, CheckReady);
+            player2 = new PlayerPanel(pl2, PanelPos.right, BotTurn, GameOver, CheckReady);
+
+            isEnded = false;
+        }
+
+        private void BotTurn()
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(0, player2.brain.notShooted.Count);
+            int i = player2.brain.notShooted[a] / 10;
+            int j = player2.brain.notShooted[a] % 10;
+
+            while (player1.brain.Play(string.Format("{0}_{1}", i, j)))
+            {
+                Thread.Sleep(500);
+
+                player2.brain.notShooted.Remove(player2.brain.notShooted[a]);
+                a = rnd.Next(0, player2.brain.notShooted.Count);
+                i = player2.brain.notShooted[a] / 10;
+                j = player2.brain.notShooted[a] % 10;
+            }
+        }
+
+        private void CheckReady()
+        {
+            if (player1.brain.state == State.ready && player2.brain.state == State.ready)
+            {
+                player1.brain.state = State.game;
+                player2.brain.state = State.game;
+
+                if (player1.playerType == PlayerType.bot)
+                    player2.Enabled = false;
+
+                else if (player2.playerType == PlayerType.bot)
+                    player1.Enabled = false;
+            }
+        }
+
+        private void GameOver()
+        {
+            player1.Enabled = false;
+            player2.Enabled = false;
+
+            isEnded = true;
+
+            if (player1.brain.isWinner)
+                player1.Victory("player2");
+
+            else
+                player2.Victory("player1");
+        }
+    }
+}
